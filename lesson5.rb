8c93085b886bd9f1f87de56d183b3d5b0e3b8ff9
satake08 <117998354+satake08@users.noreply.github.com>
2022-12-04 10:44:54 +0000

    [finish]2-5

---
+++ lesson5.rb
@@ -0,0 +1,21 @@
+total_price = 100
+
+#if total_price == 100
+#  puts "みかんを購入。所持金は0円。"
+#elsif total_price > 100
+#  puts "みかんを購入。所持金に余りあり。"
+#else
+#  puts "みかんを購入することができません。"
+#end
+#自分の回答
+
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円。"
+else
+  puts "みかんを購入することができません。"
+end
+#正の回答
+
+#どちらも「みかんを購入。所持金は0円。」で出力
